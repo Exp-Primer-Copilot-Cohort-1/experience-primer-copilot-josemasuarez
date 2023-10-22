5c09997b629369997418a4524365ee1b3b3223d4
José María <josemasuarez@gmail.com>
2023-10-22 22:03:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember () {
+    var skills = {
+        name: 'John',
+        age: 30,
+        skills: ['JS', 'CSS', 'HTML']
+    };
+    var skills2 = {
+        name: 'John',
+        age: 30,
+        skills: ['JS', 'CSS', 'HTML']
+    };
+    console.log(skills);
+    console.log(skills2);
+}
